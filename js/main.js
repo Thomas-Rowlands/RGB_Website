44076976dfd1102d998a0f9f6a5559dc046c9e2d
Thomas Rowlands <thomas.s.rowlands@gmail.com>
2023-06-06 07:03:04 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('#room-colour-picker').colorpicker({
+    component: '.btn',
+    container: "#room-colour-picker",
+    format: "rgb"
+  });
+  $('#greg-colour-picker').colorpicker({
+    component: '.btn',
+    container: "#greg-colour-picker",
+    format: "rgb"
+  });
+  $('#patrick-colour-picker').colorpicker({
+    component: '.btn',
+    container: "#patrick-colour-picker",
+    format: "rgb"
+  });
+  $('#rini-colour-picker').colorpicker({
+    component: '.btn',
+    container: "#rini-colour-picker",
+    format: "rgb"
+  });
+  $('#thomas-colour-picker').colorpicker({
+    component: '.btn',
+    container: "#thomas-colour-picker",
+    format: "rgb"
+  });
+});
